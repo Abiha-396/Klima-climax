76626a77fd327a70b08b077101a4dd2a082009d8
Abiha-396 <r24180@queens.school.nz>
2025-07-31 16:41:11 +1200

    Added a platform

--- klima-climax/Scripts/start_screen.gd
+++ klima-climax/Scripts/start_screen.gd
@@ -9,3 +9,14 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
+
+
+
+
+
+func _on_texture_button_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/menu_screen.tscn")
