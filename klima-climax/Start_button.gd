b2bc1b5c59ccf180d16d4e1d3403d1e816e681bf
Abiha-396 <r24180@queens.school.nz>
2025-08-04 10:56:03 +1200

    Making the sprite move

--- klima-climax/Start_button.gd
+++ klima-climax/Start_button.gd
@@ -12,4 +12,4 @@ func _process(delta: float) -> void:
 
 
 func _on_texture_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Level_1_Background.tscn")
+	get_tree().change_scene_to_file("res://Scenes/level_1.tscn")
