57efd6de69941e5335a0043bcb3cb0e99e0f8a81
Abiha-396 <r24180@queens.school.nz>
2025-08-28 15:04:53 +1200

    Fixing the trophy tbc

--- klima-climax/Scenes/level_1.gd
+++ klima-climax/Scenes/level_1.gd
@@ -6,15 +6,9 @@ func _ready() -> void:
 	pass # Replace with function body.
 	
 
-	
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 	
 	
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	trophy.play("Win")
-	await get_tree().create_timer(0.75).timeout
-	get_tree().change_scene_to_file("res://Scenes/level_2_platform.tscn")
+ 
