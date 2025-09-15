f85be0115c0d37e8182f8185632f0f824c7dced7
Abiha-396 <r24180@queens.school.nz>
2025-09-15 14:58:03 +1200

    Trophy works but the coins don't!

--- klima-climax/Scripts/Trophy.gd
+++ klima-climax/Scripts/Trophy.gd
@@ -1,5 +1,7 @@
 extends Node2D
-@onready var trophy= $Trophy
+@onready var trophy = $Trophy2
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,10 +12,9 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 	
-	
- 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	trophy.play("Win")
 	await get_tree().create_timer(0.75).timeout
-	get_tree().change_scene_to_file("res://Scenes/level_2_platform.tscn")
+	get_tree().change_scene_to_file("res://Scenes/end_screen.tscn")
+	
