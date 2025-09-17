a14b3771540c03b3fb9a54a49c94eef63b57a41b
Abiha-396 <r24180@queens.school.nz>
2025-09-17 13:12:56 +1200

    Fixed the end scene. Made LEVEL 1 the main scene.

--- klima-climax/Scripts/end_screen.gd
+++ klima-climax/Scripts/end_screen.gd
@@ -8,7 +8,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	label.text= "Game Over"
+	label.text= "You won"
 	
 
 	
--- klima-climax/end_screen_lost.gd
+++ klima-climax/end_screen_lost.gd
@@ -1,15 +1,11 @@
 extends Control
-
+@onready var label = $Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
-
-
-func _on_texture_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/LEVEL 1.tscn")
+	label.text= "Game Over"
