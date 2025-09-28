a6bc04205627822b1371a1fc8947107a85dcd4ca
Abiha-396 <r24180@queens.school.nz>
2025-09-28 18:06:11 +1300

    The elf is not dying!!!
    The bats collide with my elf, and the scene is reloaded, but that also means my lives variable would never go less than 3-1! An example of how nothing works.

--- klima-climax/Scripts/Global script.gd
+++ klima-climax/Scripts/Global script.gd
@@ -1,6 +1,15 @@
 extends Node
 
 var score =0
+var life: int = 3
 
 func _ready() -> void:
-	print(score)
+	pass
+
+
+	
+	
+func _on_bat_elf_hit():
+	life-=1
+	print(life)
+	get_tree().reload_current_scene()
--- klima-climax/bat.gd
+++ klima-climax/bat.gd
@@ -1,28 +1,35 @@
 extends Node2D
 
 @onready var bat = $Bat
-@onready var label=$Label
-var life = 3
-var Life=str(life)
 
+signal elf_hit
+
+func _ready():
+	GlobalScript.life=3
+	
+func _process(delta):
+	if GlobalScript.life<1:
+		get_tree().change_scene_to_file("res://Scenes/end_screen_lost.tscn")
+		
 	
 	
 func _on_timer_timeout():
 	var clone = bat.duplicate()
 	add_child(clone)
-	clone.position = Vector2(randf_range(0,5500), randf_range(0,350))
+	clone.position = Vector2(randf_range(0,6000), randf_range(0,400))
+
 
 
 func _on_area_2d_body_entered(body):
-	queue_free()
-	await get_tree().create_timer(0.05).timeout
-	get_tree().reload_current_scene()
-	life-=1
+	self.queue_free()
+	elf_hit.emit()
 	
-func _physics_process(delta):
-	#label.text = Life
-	if life<1:
-		get_tree().change_scene_to_file("res://Scenes/end_screen_lost.tscn")
+	
+	
+	
+
+	
+	
 		
 	
 	
