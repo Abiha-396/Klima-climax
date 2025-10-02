c5c0d5f39067196bfba0564d47c85a24b9be65dc
Abiha-396 <r24180@queens.school.nz>
2025-10-03 10:24:54 +1300

    Added level 4
    The platform goes up, down and straight and provides a frictionless sliding effect. But that doesn't go well with my sprite animation, so I am making it up and straight, with moving platforms.

---
+++ klima-climax/Scripts/arrow.gd
@@ -0,0 +1,32 @@
+extends Area2D
+
+@export var speed : int =200
+var direction: Vector2
+@export var Ref_bat: PackedScene
+
+func _physics_process(delta):
+	# Adjusts the arrow direction 
+	direction= Vector2.RIGHT.rotated(rotation)
+	global_position += direction*speed*delta
+
+func destroy():
+	queue_free()
+
+
+
+
+func _on_area_entered(area):
+	print(area)
+	destroy()
+
+
+
+func _on_body_entered(body):
+	print(body)
+	destroy()
+
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	print("I exited")
+	destroy()
--- klima-climax/Scripts/player_elf_real.gd
+++ klima-climax/Scripts/player_elf_real.gd
@@ -4,21 +4,23 @@ extends CharacterBody2D
 @onready var Bat= $RayCast2D
 @export	var gravity =400
 @export var JUMP_VELOCITY=-200
+@export var Ref_arrow: PackedScene
 const SPEED= 250
+var isAttacking: bool=false
+@export var rotation_speed = 10.0
+
 
 #func die():
 	#animated_sprite.play("Death")
 	#get_tree().change_scene_to_file("res://Scenes/end_screen.tscn")
 
-func _process(delta):
-	if Bat.is_colliding():
-		var bat = Bat.get_collider()
-		print(bat)
+
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	
 
 	# Handle jump.
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
@@ -78,7 +80,10 @@ func _physics_process(delta: float) -> void:
 			animated_sprite.play("Idle")
 			animated_sprite.flip_h =false
 		
-		
+	if Input.is_action_just_pressed("Attack"):
+		shoot_arrow()
+	
+	
 	
 	#if global_position.y>100:
 		#die()
@@ -93,6 +98,19 @@ func _on_killzone_body_entered(body: Node2D) -> void:
 	get_tree().reload_current_scene()
 	
 
+func shoot_arrow():
+	print("Arrow_shot")
+	if Ref_arrow:
+		var arrow= Ref_arrow.instantiate()
+		get_tree().current_scene.add_child(arrow)
+		arrow.global_position= self.global_position
+		
+		var arrow_rotation= self.global_position.direction_to(get_global_mouse_position()).angle()
+		arrow.rotation=arrow_rotation
+	
+	
+	
 
 
+	
 	
